2d2659b8e4e6cc85163cb2de63e11219aa1720ec
moh715 <mohamedyn77@gmail.com>
2026-07-26 08:40:45 +0300

    update single_move_benchmark

--- benchmark.py
+++ benchmark.py
@@ -42,7 +42,7 @@ def single_move_benchmark(depth, eval):
     stats.print_stats(25)
     if searcher.tt_lookups:
         print("TT hit rate:", round(100 * searcher.tt_hits / searcher.tt_lookups, 2), "%")
-
+    print(f"score: {score} move:{move}")
 
 def game_benchmark(depth, eval):
     board = bc.Board()
@@ -250,4 +250,4 @@ def eval_vs_eval2(depth, games, eval_fns=[Handcrafted, NNEvaluation]):
             "%"
         )
 
-single_move_benchmark(10, Handcrafted)
+single_move_benchmark(7, Handcrafted)
